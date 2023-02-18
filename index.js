3f3a77e803dc22ab9c5743349f43b95e4a777316
E.Borisov <borisov.rzd@yandex.ru>
2023-02-18 14:23:51 +0300

    +like +popup(correct)

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/*назначены 3-и id на "Карандаш" редактирования профиля, "Крест" закрытия popup и сам "popup" 
+открытие/закрытие происходит за счёт селектора popup_opened */ 
 const editProfileOpenButton = document.querySelector('#open-edit-popup');
 const editProfileCloseButton = document.querySelector('#close-edit-popup')
 const editPopup = document.querySelector('#edit-popup');
@@ -9,7 +11,14 @@ editProfileOpenButton.addEventListener('click', function () {
 editProfileCloseButton.addEventListener('click', function() {
   editPopup.classList.add('popup_opened')
 }); 
+/*
+ */
 
+
+
+
+/*назначены константы на поля профиля, они "привязаны" к полям формы редактирования
+при изменении данных в форме, происходит редактирования профиля */ 
 const userName = 'Жак-Ив Кусто';
 const userOccupation = 'Исследователь океана';
 
