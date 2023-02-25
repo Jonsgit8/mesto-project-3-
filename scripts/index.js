325c198baf45687a53572c170d8a7a54d893328f
E.Borisov <borisov.rzd@yandex.ru>
2023-02-25 13:57:01 +0300

    ver_3

--- scripts/index.js
+++ scripts/index.js
@@ -4,12 +4,11 @@ const userOccupationElement = document.querySelector('.profile__occupation');/*2
 const editPopUp = document.querySelector ('.popup');/*попап*/
 const editProfileOpenButton = document.querySelector('.profile__correct');/*карандаш профиля*/   /* */
 
-const userNameInput = document.querySelector('.form__name');/*1-ая строка формы*/
-const userOccupationInput = document.querySelector('.form__occupation');/*2-ая строка формы*/
+const userNameInput = document.querySelector('.form__input_name');/*1-ая строка формы*/
+const userOccupationInput = document.querySelector('.form__input_occupation');/*2-ая строка формы*/
 
 /*реализация функционала открытия попап и заполнения полей формы*/
 editProfileOpenButton.addEventListener('click', function(event) {
-  console.log(editProfileOpenButton);
   editPopUp.classList.add('popup_opened');/*move класс*/
   userNameInput.value = userNameElement.textContent;
   userOccupationInput.value = userOccupationElement.textContent;
@@ -17,9 +16,11 @@ editProfileOpenButton.addEventListener('click', function(event) {
 
 /*реализация функционала при нажатии на Крестик попап*/
 const editProfileCloseButton = document.querySelector('.popup__close');/*Крестик попап*/
-
-editProfileCloseButton.addEventListener('click', function(event) {
+function closePopUp() {
   editPopUp.classList.remove('popup_opened');/*move класс*/
+}
+editProfileCloseButton.addEventListener('click', function(event) {
+  closePopUp()/*move класс*/
 });
 
 /*реализация функционала Сохранения формы*/
@@ -29,5 +30,5 @@ formElementInput.addEventListener('submit', function (event) {
   event.preventDefault();
   userNameElement.textContent = userNameInput.value
   userOccupationElement.textContent =  userOccupationInput.value;
-  editPopUp.classList.remove('popup_opened');
+  closePopUp()
 })
