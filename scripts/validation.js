e0dfb5166f36769fdda1eb36ac75729098054292
E.Borisov <borisov.rzd@yandex.ru>
2023-03-21 09:05:50 +0800

    use syntax JS

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,40 +1,38 @@
 /*показ ошибок инпутов*/
 const showInputError = (input, errorTextElement, validationMessage, inputErrorClass) => {
   errorTextElement.textContent = validationMessage;
-  errorTextElement.classList.add('form__error')
-  input.classList.add(inputErrorClass)
-}
+  errorTextElement.classList.add('form__error');
+  input.classList.add(inputErrorClass);
+};
 /*скрытие ошибок инпутов*/
 const hideInputError = (input, errorTextElement, inputErrorClass) => {
-  errorTextElement.textContent = ''
-  input.classList.remove(inputErrorClass)
-}
+  errorTextElement.textContent = '';
+  input.classList.remove(inputErrorClass);
+};
 
 /*проверка инпутов на валидность*/
 function checkInvalidInputs(inputList) {
-  return inputList.some((input) => !input.validity.valid)
+  return inputList.some((input) => !input.validity.valid);
 }
 
 /*disabled для Button*/
 function setDisabledButton(button, inactiveButtonClass) {
-  button.classList.add(inactiveButtonClass)
-  button.setAttribute('disabled', true)
+  button.classList.add(inactiveButtonClass);
+  button.setAttribute('disabled', true);
 }
 /*enabled для Button*/
 function setEnabledButton(button, inactiveButtonClass) {
-  button.classList.remove(inactiveButtonClass)
-  button.removeAttribute('disabled')
-  button.disabled = false
+  button.classList.remove(inactiveButtonClass);
+  button.removeAttribute('disabled');
+  button.disabled = false;
 }
 
 /*переключение состояния кнопки Cохранить*/
 function toggleButtonState(inputList, button, inactiveButtonClass) {
     if(checkInvalidInputs(Array.from(inputList))) {
-      setDisabledButton(button, inactiveButtonClass)
-      console.log('LBP')
+      setDisabledButton(button, inactiveButtonClass);
     } else {
-      setEnabledButton(button, inactiveButtonClass)
-      console.log('FFF')
+      setEnabledButton(button, inactiveButtonClass);
     }
 }
 
@@ -42,36 +40,36 @@ function toggleButtonState(inputList, button, inactiveButtonClass) {
 const checkInputValidity = (input, errorClass, inputErrorClass, form, submitButtonSelector) => {
   // const button = form.querySelector(submitButtonSelector)
   // .form__error_user-name-input
-  const errorTextElement = document.querySelector(`${errorClass}${input.id}`)
+  const errorTextElement = document.querySelector(`${errorClass}${input.id}`);
   if(!input.validity.valid) {
-     showInputError(input, errorTextElement, input.validationMessage, inputErrorClass)
+     showInputError(input, errorTextElement, input.validationMessage, inputErrorClass);
     } else {
-      hideInputError(input, errorTextElement, inputErrorClass)
+      hideInputError(input, errorTextElement, inputErrorClass);
     }
-  }
+  };
 /*прослушивание валидности полей*/
 const setEventListeners = (form, config, errorClass, inputErrorClass, submitButtonSelector, inactiveButtonClass) => {
-  const inputList = form.querySelectorAll(config.inputSelector)
-  const button = form.querySelector(submitButtonSelector)
-  toggleButtonState(inputList, button, inactiveButtonClass)
+  const inputList = form.querySelectorAll(config.inputSelector);
+  const button = form.querySelector(submitButtonSelector);
+  toggleButtonState(inputList, button, inactiveButtonClass);
 
   inputList.forEach((input) => {
     input.addEventListener('input', (e) => {
-      checkInputValidity(input, errorClass, inputErrorClass, form, submitButtonSelector)
-      toggleButtonState(inputList, button, inactiveButtonClass)
-    })
+      checkInputValidity(input, errorClass, inputErrorClass, form, submitButtonSelector);
+      toggleButtonState(inputList, button, inactiveButtonClass);
+    });
   });
-}
+};
 /*запуск валидации*/
 const enableValidation = (config) => {
-  const formList = document.querySelectorAll(config.formSelector)
+  const formList = document.querySelectorAll(config.formSelector);
   formList.forEach((form) => {
     form.addEventListener('submit', (e) => {
-      e.preventDefault(e)
-    })
-    setEventListeners(form, config, config.errorClass, config.inputErrorClass, config.submitButtonSelector, config.inactiveButtonClass)
-  })
-}
+      e.preventDefault(e);
+    });
+    setEventListeners(form, config, config.errorClass, config.inputErrorClass, config.submitButtonSelector, config.inactiveButtonClass);
+  });
+};
 
 /*настройки валидации*/
 const config = {
@@ -81,6 +79,6 @@ const config = {
   inactiveButtonClass: 'form__button_disabled',
   inputErrorClass: 'form__input_error', // __ red
   errorClass: '.form__error_'
-}
+};
 
 enableValidation(config);
